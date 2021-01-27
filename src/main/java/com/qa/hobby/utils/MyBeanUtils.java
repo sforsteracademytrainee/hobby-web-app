25ec8d9ebdf6a43fd4f6c41abcdc1ed56bb4b6f4
Simon Forster <sforster@academytrainee.com>
2021-01-27 12:03:11 +0000

    Added MyBeanUtils.java

---
+++ src/main/java/com/qa/hobby/utils/MyBeanUtils.java
@@ -0,0 +1,27 @@
+package com.qa.hobby.utils;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class MyBeanUtils { 
+	
+	private MyBeanUtils() { }
+	public static void mergeNotNull(Object source, Object target) {
+		BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
+	} 
+	
+	private static String[] getNullPropertyNames(Object source) {
+		final BeanWrapper src = new BeanWrapperImpl(source);
+		Set<String> names = new HashSet<>();
+			for (PropertyDescriptor pd : src.getPropertyDescriptors()) {
+				if (src.getPropertyValue(pd.getName()) == null)names.add(pd.getName());
+			}
+			return names.toArray(new String[names.size()]);
+		}
+
+}
