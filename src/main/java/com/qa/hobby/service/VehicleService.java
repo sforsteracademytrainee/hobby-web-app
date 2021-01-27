63f3b115768bd00d79913b69bfc6176ed7ce8f95
Simon Forster <sforster@academytrainee.com>
2021-01-27 11:20:33 +0000

    [PRO2-17] Created the vehicle service

---
+++ src/main/java/com/qa/hobby/service/VehicleService.java
@@ -0,0 +1,53 @@
+package com.qa.hobby.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import com.qa.hobby.persistence.domain.VehicleDomain;
+import com.qa.hobby.persistence.dto.VehicleDTO;
+import com.qa.hobby.persistence.repos.VehicleRepo;
+import com.qa.hobby.utils.MyBeanUtils;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class VehicleService {
+	
+	private VehicleRepo repo;
+	private ModelMapper mapper;
+	
+	private VehicleDTO mapToDTO(VehicleDomain model) {
+		return this.mapper.map(model, VehicleDTO.class);
+	}
+	
+	// Create
+	public VehicleDTO create(VehicleDomain model) {
+		return this.mapToDTO(this.repo.save(model));
+	}
+	
+	// Read
+	public VehicleDTO read(Long id) {
+		return this.mapToDTO(this.repo.findById(id).orElseThrow());
+	}
+	
+	public List<VehicleDTO> readAll() {
+		return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+	}
+	
+	// Update
+	public VehicleDTO update(Long id, VehicleDomain model) {
+		VehicleDomain updatedModel = this.repo.findById(id).orElseThrow();
+		MyBeanUtils.mergeNotNull(model, updatedModel);
+		return this.mapToDTO(this.repo.save(updatedModel));
+	}
+	
+	// Delete
+	public boolean delete(Long id) {
+		this.repo.deleteById(id);
+		return !this.repo.existsById(id);
+	}
+}
