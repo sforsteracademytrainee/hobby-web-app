1e85940336db06cbad4b08aba10bc8382d28fa75
Simon Forster <sforster@academytrainee.com>
2021-01-28 10:03:39 +0000

    [PRO2-25] Implemented the delete function in the person-read page

--- src/main/resources/static/js/people-read.js
+++ src/main/resources/static/js/people-read.js
@@ -4,7 +4,11 @@ const idBox = document.querySelector("#personId");
 const firstNameBox = document.querySelector("#personFirstName");
 const surnameBox = document.querySelector("#personSurname");
 
-const searchPerson = () => {
+// Maybe give a page when the person does not exist
+
+// ^^^ need to handle when the lookup doesnt exist
+
+const getParams = () => {
 	let queryStr = window.location.search;
 	let paramPairs = queryStr.substr(1).split('&');
 	
@@ -14,31 +18,61 @@ const searchPerson = () => {
 	    params[parts[0]] = parts[1];
 	}
 	
+	return params;
+}
+
+const searchPerson = () => {
+	let params = getParams();
+	
 	if (params.id !== undefined && params.id !== null && params.id !== "NaN") {
-		idBranch(params.id);
-	} else if (params.firstName != null && params.surname != null) {
-		console.log("do name search");
-	} else {
-		console.log("not enough data to search");
+		idSearch(params.id);
 	}
 }
 
-const idBranch = (id) => {
-	
+const idSearch = (id) => {
 	fetch("person/read/"+id)
-	.then((response) => response.json())
+	.then((response) => {
+		if (response.status === 200) {
+			return response.json();
+		} else {
+			results.innerHTML = "Person does not exist.";
+		}
+	})
 	.then((json) => {
 		results.innerHTML = json.firstName + " " +json.surname + " " + json.address + " " + json.phone;
+		let button = document.createElement("button");
+		button.setAttribute("onClick", "idDelete("+id+");");
+		button.setAttribute("class", "btn btn-danger");
+		button.innerHTML = "Delete";
+		results.appendChild(button);
 	}) 
+}
+
+const deletePerson = () => {
+	let params = getParams();
+	
+	if (params.id !== undefined && params.id !== null && params.id !== "NaN") {
+		idDelete(params.id);
+	} else {
+		console.log("Not enough data to search");
+	}
+}
+
+const idDelete = (id) => {
+	fetch("person/delete/"+id, {
+		method: "DELETE"
+	})
+	.then((response) => {
+		if (response.status === 204) {
+			results.innerHTML = "User " + id + " has succesfully been deleted.";
+		}
+	});
+	// need to make it cleaner when we do delte to make it more intuitive.
 }
 
 
 
 // Button methods
-const idSearch = () => {
+const idSubmit = () => {
 	window.location.href = "people-read.html?id=" + Number.parseInt(idBox.value);
-}
-
-const nameSearch = () => {
-	window.location.href = "people-read.html?firstName=" + firstNameBox.value + "&surname=" + surnameBox.value;
 }
